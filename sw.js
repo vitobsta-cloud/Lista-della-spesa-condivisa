24067e67ff7c86f3432225d024272861de4bfb59
vitobsta-cloud <vitobsta@gmail.com>
2026-08-28 19:22:38 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,54 @@
+const CACHE_NAME = 'spesa-costi-v12';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './chart.min.js',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      // IMPORTANTE: {cache:'reload'} forza lo scaricamento dalla rete, ignorando
+      // la cache HTTP del browser. Senza questo, il service worker rischiava di
+      // "aggiornarsi" installando comunque una copia vecchia di index.html
+      // ancora presente nella cache HTTP, restando bloccato sulla versione precedente.
+      return Promise.all(
+        ASSETS.map(url => fetch(url, {cache:'reload'}).then(response => cache.put(url, response)))
+      );
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      if (cached) return cached;
+
+      return fetch(event.request).then(response => {
+        if (!response || response.status !== 200 || response.type === 'error') {
+          return response;
+        }
+        const responseToCache = response.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, responseToCache);
+        });
+        return response;
+      }).catch(() => caches.match('./index.html'));
+    })
+  );
+});
